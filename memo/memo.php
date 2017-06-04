366cdc2a2f88b7aa87763bb19b8f8b73626a27eb
SJR\kadam <kadamon2007@gmail.com>
2017-06-05 01:25:56 +0900

    recommandation page // memo.php

--- memo/memo.php
+++ memo/memo.php
@@ -1,184 +1,139 @@
 <?
-   session_start();
-
-   $scale=5;         // 한 화면에 표시되는 글 수
-   include "../lib/dbconn.php";
-
-   $sql = "select * from memo order by num desc";
-   $result = mysql_query($sql, $connect);
-
-   $total_record = mysql_num_rows($result); // 전체 글 수
-
-   // 전체 페이지 수($total_page) 계산
-   if ($total_record % $scale == 0)
-      $total_page = floor($total_record/$scale);
-   else
-      $total_page = floor($total_record/$scale) + 1;
-
-   if (!$page)                 // 페이지번호($page)가 0 일 때
-      $page = 1;              // 페이지 번호를 1로 초기화
-
-   // 표시할 페이지($page)에 따라 $start 계산
-   $start = ($page - 1) * $scale;
-
-   $number = $total_record - $start;
+	session_start();
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-<meta charset="utf-8">
-<link href="../css/common.css" rel="stylesheet" type="text/css" media="all">
-<link href="../css/memo.css" rel="stylesheet" type="text/css" media="all">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+<link rel="stylesheet" href="../assets/css/main.css" />
 </head>
 
 <body>
-<div id="wrap">
-  <div id="header">
-    <? include "../lib/top_login2.php"; ?>
-  </div>  <!-- end of header -->
+	<div class="page-wrap">
 
-  <div id="menu">
-   <? include "../lib/top_menu2.php"; ?>
-  </div>  <!-- end of menu -->
+		<!-- nav -->
+		<nav id="nav">
+		    <? include "../lib/top_login1.php"; ?>
+		</nav>
 
-  <div id="content">
-   <div id="col1">
-      <div id="left_menu">
-<?
-         include "../lib/left_menu.php";
-?>
-      </div>
-   </div>
-   <div id="col2">
-      <div id="title">
-         <img src="../img/title_memo.gif">
-      </div>
+		<!-- Main -->
+			<section id="main">
 
-      <div id="memo_row1">
-          <form  name="memo_form" method="post" action="insert.php">
-         <div id="memo_writer"><span >▷ <?= $usernick ?></span></div>
-         <div id="memo1"><textarea rows="6" cols="95" name="content"></textarea></div>
-         <div id="memo2"><input type="image" src="../img/memo_button.gif"></div>
-      </form>
-      </div> <!-- end of memo_row1 -->
-<?
-   for ($i=$start; $i<$start+$scale && $i < $total_record; $i++)
-   {
-      mysql_data_seek($result, $i);
-      $row = mysql_fetch_array($result);
+				<!-- Banner -->
+					<section id="banner">
+						<div class="inner">
 
-     $memo_id      = $row[id];
-     $memo_num     = $row[num];
-      $memo_date    = $row[regist_day];
-     $memo_nick    = $row[nick];
+							<?
+							if(!$userid)
+								{
+									?>
+                  <h1>Recommendation</h1>
+							    <p>저희 홈페이지에 대해 건의사항을 남겨주세요!</p>
+							<?
+							}
+							else
+							{
+							?>
+                <h1>Recommendation</h1>
+								<p><?=$usernick?>(Lev<?=$userlevel?>) 님 저희 홈페이지에 대해 건의사항을 남겨주세요!</p>
+							<?
+							}
+							?>
+							<ul class="actions">
+								<li><a href="#galleries" class="button alt scrolly big">바로보기</a></li>
+							</ul>
+						</div>
+					</section>
 
-     $memo_content = str_replace("\n", "<br>", $row[content]);
-     $memo_content = str_replace(" ", "&nbsp;", $memo_content);
-?>
-      <div id="memo_writer_title">
-      <ul>
-      <li id="writer_title1"><?= $number ?></li>
-      <li id="writer_title2"><?= $memo_nick ?></li>
-      <li id="writer_title3"><?= $memo_date ?></li>
-      <li id="writer_title4">
-            <?
-               if($userid=="admin" || $userid==$memo_id)
-                   echo "<a href='delete.php?num=$memo_num'>[삭제]</a>";
-           ?>
-      </li>
-      </ul>
-      </div>
-      <div id="memo_content"><?= $memo_content ?>
-      </div>
-      <div id="ripple">
-         <div id="ripple1">덧글</div>
-         <div id="ripple2">
-<?
-       $sql = "select * from memo_ripple where parent='$memo_num'";
-       $ripple_result = mysql_query($sql);
+				<!-- Gallery -->
+					<section>
 
-      while ($row_ripple = mysql_fetch_array($ripple_result))
-      {
-         $ripple_num     = $row_ripple[num];
-         $ripple_id      = $row_ripple[id];
-         $ripple_nick    = $row_ripple[nick];
-         $ripple_content = str_replace("\n", "<br>", $row_ripple[content]);
-         $ripple_content = str_replace(" ", "&nbsp;", $ripple_content);
-         $ripple_date    = $row_ripple[regist_day];
-?>
-            <div id="ripple_title">
-            <ul>
-            <li><?= $ripple_nick ?> &nbsp;&nbsp;&nbsp; <?= $ripple_date ?></li>
-            <li id="mdi_del">
-               <?
-                  if($userid=="admin" || $userid==$ripple_id)
-                        echo "<a href='delete_ripple.php?num=$ripple_num'>삭제</a>";
-               ?>
-            </li>
-            </ul>
-            </div>
-            <div id="ripple_content"> <?= $ripple_content ?></div>
-<?
-      }
-?>
-            <form  name="ripple_form" method="post" action="insert_ripple.php">
-            <input type="hidden" name="num" value="<?= $memo_num ?>">
-            <div id="ripple_insert">
-                <div id="ripple_textarea">
-                  <textarea rows="3" cols="80" name="ripple_content"></textarea>
-               </div>
-               <div id="ripple_button"><input type="image" src="../img/memo_ripple_button.png"></div>
-            </div>
-            </form>
+						<!-- Photo Galleries -->
+							<div class="inner">
+								<header class="special">
+									<h2>What's New</h2>
+								</header>
+                <div id="disqus_thread"></div>
+                <script>
 
-         </div> <!-- end of ripple2 -->
-            <div class="clear"></div>
-         <div class="linespace_10"></div>
-<?
-      $number--;
-    }
-    mysql_close();
-?>
-         <div id="page_num"> ◀ 이전 &nbsp;&nbsp;&nbsp;&nbsp;
-<?
-   // 게시판 목록 하단에 페이지 링크 번호 출력
-   for ($i=1; $i<=$total_page; $i++)
-   {
-      if ($page == $i)     // 현재 페이지 번호 링크 안함
-      {
-         echo "<b> $i </b>";
-      }
-      else
-      {
-         echo "<a href='memo.php?page=$i'> $i </a>";
-      }
-   }
-?>
-         &nbsp;&nbsp;&nbsp;&nbsp;다음 ▶</div>
-       </div> <!-- end of ripple -->
-   </div> <!-- end of col2 -->
-  </div> <!-- end of content -->
-</div> <!-- end of wrap -->
-<div id="disqus_thread"></div>
-  <script>
+                /**
+                *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+                *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+                /*
+                var disqus_config = function () {
+                this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+                this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+                };
+                */
+                (function() { // DON'T EDIT BELOW THIS LINE
+                var d = document, s = d.createElement('script');
+                s.src = 'https://phptermproject.disqus.com/embed.js';
+                s.setAttribute('data-timestamp', +new Date());
+                (d.head || d.body).appendChild(s);
+                })();
+                </script>
+                <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+              </div>
+					</section>
+				<!--
 
-  /**
-  *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
-  *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
-  /*
-  var disqus_config = function () {
-  this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
-  this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-  };
-  */
-  (function() { // DON'T EDIT BELOW THIS LINE
-  var d = document, s = d.createElement('script');
-  s.src = 'https://phptermproject.disqus.com/embed.js';
-  s.setAttribute('data-timestamp', +new Date());
-  (d.head || d.body).appendChild(s);
-  })();
-  </script>
-  <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+					<section id="contact">
+
+							<div class="social column">
+								<h3>About Me</h3>
+								<p>Mus sed interdum nunc dictum rutrum scelerisque erat a parturient condimentum potenti dapibus vestibulum condimentum per tristique porta. Torquent a ut consectetur a vel ullamcorper a commodo a mattis ipsum class quam sed eros vestibulum quisque a eu nulla scelerisque a elementum vestibulum.</p>
+								<p>Aliquet dolor ultricies sem rhoncus dolor ullamcorper pharetra dis condimentum ullamcorper rutrum vehicula id nisi vel aptent orci litora hendrerit penatibus erat ad sit. In a semper velit eleifend a viverra adipiscing a phasellus urna praesent parturient integer ultrices montes parturient suscipit posuere quis aenean. Parturient euismod ultricies commodo arcu elementum suspendisse id dictumst at ut vestibulum conubia quisque a himenaeos dictum proin dis purus integer mollis parturient eros scelerisque dis libero parturient magnis.</p>
+								<h3>Follow Me</h3>
+								<ul class="icons">
+									<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+									<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+									<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+								</ul>
+							</div>
+
+
+							<div class="column">
+								<h3>Get in Touch</h3>
+								<form action="#" method="post">
+									<div class="field half first">
+										<label for="name">Name</label>
+										<input name="name" id="name" type="text" placeholder="Name">
+									</div>
+									<div class="field half">
+										<label for="email">Email</label>
+										<input name="email" id="email" type="email" placeholder="Email">
+									</div>
+									<div class="field">
+										<label for="message">Message</label>
+										<textarea name="message" id="message" rows="6" placeholder="Message"></textarea>
+									</div>
+									<ul class="actions">
+										<li><input value="Send Message" class="button" type="submit"></li>
+									</ul>
+								</form>
+							</div>
+
+					</section>
+				-->
+				<!-- Footer -->
+					<footer id="footer">
+						<div class="copyright">
+							&copy; Untitled Design: <a href="https://templated.co/">TEMPLATED</a>. Images: <a href="https://unsplash.com/">Unsplash</a>.
+						</div>
+					</footer>
+			</section>
+	</div>
+
+	<!-- Scripts -->
+		<script src="assets/js/jquery.min.js"></script>
+		<script src="assets/js/jquery.poptrox.min.js"></script>
+		<script src="assets/js/jquery.scrolly.min.js"></script>
+		<script src="assets/js/skel.min.js"></script>
+		<script src="assets/js/util.js"></script>
+		<script src="assets/js/main.js"></script>
+
+
 
 
 </body>
